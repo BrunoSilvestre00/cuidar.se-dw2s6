73767b05f85221766579626bba15d68f20eb2f79
BrunoSilvestre00 <bruno.ap.silvestre@gmail.com>
2022-12-01 11:50:20 -0300

    update allowed hosts

--- config/security.py
+++ config/security.py
@@ -3,11 +3,11 @@ from decouple import config as env
 SECRET_KEY = env('SECRET_KEY')
 
 HOSTS = [
-    '127.0.0.1:8000', '127.0.0.1:3000', 'localhost:3000'
+    '127.0.0.1:8000', '127.0.0.1:3000', 'localhost:3000', '25.28.197.128:8000'
 ]
 
 ALLOWED_HOSTS = [
-    '127.0.0.1', 'localhost'
+    '127.0.0.1', 'localhost', '25.28.197.128'
 ]
 
 CSRF_TRUSTED_ORIGINS = [f'http://{host}' for host in HOSTS]
